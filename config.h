b42512714f1c9295c9a153dff88c9b476f3583b6
Jonathan Rothberg <jon@join.build>
2026-02-07 19:28:28 -0800

    Reduce window gaps from 8px to 4px

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows (XMonad: myBorderWidth = 2) */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int gappx              = 8;        /* gap pixel between windows (XMonad: myWindowSpacing = 8) */
+static const int gappx              = 4;        /* gap pixel between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
